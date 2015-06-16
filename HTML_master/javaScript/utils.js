040f0970c37c04bf1dec78781b3c07876593e82a
Chawata <julien.husch@hotmail.fr>
2015-06-16 21:09:11 +0200

    Update utils.js
    ajout de la fonction timeStampToDate

--- HTML_master/javaScript/utils.js
+++ HTML_master/javaScript/utils.js
@@ -1,10 +1,17 @@
 $(function () {
 	
-	function timeStamp(var maDate)
+	function dateTotimeStamp(var maDate)
 	{
 		maDate = maDate.split("/");
-		var nouvelleDate = maDate[1] + "," + maDate[0] + "," + maDate[2];
+		var nouvelleDate = maDate[1] + "/" + maDate[0] + "/" + maDate[2];
 		var timeStampDate = nouvelleDate.getTime();
 		return timeStampDate;
 	}
+	
+	function timeStampToDate(var timestamp)
+	{
+		var maDate = new Date();
+		maDate.setTime(timestamp);
+	}
+
 });
