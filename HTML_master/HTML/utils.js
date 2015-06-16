69e1fd514bc1d3ba419057b6aa1a100bbfd72261
Chawata <julien.husch@hotmail.fr>
2015-06-16 20:12:50 +0200

    Fichier utils.js contenant les fonctions utiles

---
+++ HTML_master/HTML/utils.js
@@ -0,0 +1,10 @@
+$(function () {
+	
+	function timeStamp(var maDate)
+	{
+		maDate = maDate.split("-");
+		var nouvelleDate = maDate[1] + "," + maDate[0] + "," + maDate[2];
+		var timeStampDate = nouvelleDate.getTime();
+		return timeStampDate;
+	}
+});
