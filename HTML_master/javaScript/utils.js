26362cce8e1df7e806fdc2786062f61ebf5f77b3
Chawata <julien.husch@hotmail.fr>
2015-06-21 19:41:30 +0200

    Update utils.js
    Mise a jour des fonctions

--- HTML_master/javaScript/utils.js
+++ HTML_master/javaScript/utils.js
@@ -1,17 +1,24 @@
 $(function () {
 	
-	function dateTotimeStamp(var maDate)
+	console.log(dateTotimeStamp("20/06/2015"));
+	console.log(timeStampToDate("1274392800"));
+	
+	function dateTotimeStamp(maDate)
 	{
+		maDate = maDate + ''
 		maDate = maDate.split("/");
-		var nouvelleDate = maDate[1] + "/" + maDate[0] + "/" + maDate[2];
-		var timeStampDate = nouvelleDate.getTime();
+		var nouvelleDate = maDate[1] + "-" + maDate[0] + "-" + maDate[2];
+		var date = new Date(nouvelleDate);
+		var timeStampDate = (date.getTime())/1000;
 		return timeStampDate;
 	}
 	
-	function timeStampToDate(var timestamp)
+	function timeStampToDate(timestamp)
 	{
-		var maDate = new Date();
-		maDate.setTime(timestamp);
+		var date = new Date(timestamp*1000);
+		return date;
 	}
-
+	
+	
+	
 });
