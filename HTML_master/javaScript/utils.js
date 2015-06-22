9b6eb249d6b022bd666517b8b8b4bb602297f581
Chawata <julien.husch@hotmail.fr>
2015-06-22 16:58:21 +0200

    Update utils.js

--- HTML_master/javaScript/utils.js
+++ HTML_master/javaScript/utils.js
@@ -1,13 +1,13 @@
 $(function () {
 	
-	console.log(dateTotimeStamp("20/06/2015"));
-	console.log(timeStampToDate("1274392800"));
+	console.log(dateTotimeStamp("06-20-2015"));
+	console.log(timeStampToDate("1434499200"));
 	
 	function dateTotimeStamp(maDate)
 	{
 		maDate = maDate + ''
-		maDate = maDate.split("/");
-		var nouvelleDate = maDate[1] + "-" + maDate[0] + "-" + maDate[2];
+		maDate = maDate.split("-");
+		var nouvelleDate = maDate[0] + "-" + maDate[1] + "-" + maDate[2];
 		var date = new Date(nouvelleDate);
 		var timeStampDate = (date.getTime())/1000;
 		return timeStampDate;
@@ -18,6 +18,25 @@ $(function () {
 		var date = new Date(timestamp*1000);
 		return date;
 	}
+	//A terminer par rapport au formulaire
+	$('#valider').mousedown(function()
+	{
+		if($('#other-date').is(':checked'))
+		{
+			var datedebut = $('#date_debut').val();
+			var datefin = $('#date_fin').val();
+			console.log(dateTotimeStamp(datedebut));
+			console.log(dateTotimeStamp(datefin));
+		}
+		if($('#per-year').is(':checked'))
+		{
+			var date = $('#date_annee').val();
+			console.log(date);
+			var dateannee = "01-" + "01-" + date;
+			console.log(dateTotimeStamp(dateannee));
+		}
+		if($(''))
+	});
 	
 	
 	
