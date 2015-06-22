e7970d4d4d69e62b495dbfcb7f386bc43fbce71b
Chawata <julien.husch@hotmail.fr>
2015-06-22 21:30:58 +0200

    Update utils.js

--- HTML_master/javaScript/utils.js
+++ HTML_master/javaScript/utils.js
@@ -1,7 +1,11 @@
 $(function () {
 	
+	var timeStamptest = 1433109600
 	console.log(dateTotimeStamp("06-20-2015"));
-	console.log(timeStampToDate("1434499200"));
+	console.log(timeStampToDate(timeStamptest));
+	var timeStampDay = timeStampIdenticday(timeStamptest)
+	console.log(timeStampDay);
+	console.log(timeStampToDate(timeStampDay));
 	
 	function dateTotimeStamp(maDate)
 	{
@@ -18,13 +22,52 @@ $(function () {
 		var date = new Date(timestamp*1000);
 		return date;
 	}
+	
+	function timeStampIdenticday(timestamp)
+	{
+		var nouveautimeStamp = timestamp + 86399;
+		return nouveautimeStamp;
+	}
+	
+	function getXMLHttpRequest() 
+	{
+		var xhr = null;
+		
+		if (window.XMLHttpRequest || window.ActiveXObject) 
+		{
+			if (window.ActiveXObject) 
+			{
+				try 
+				{
+					xhr = new ActiveXObject("Msxml2.XMLHTTP");
+				} 
+				catch(e) 
+				{
+					xhr = new ActiveXObject("Microsoft.XMLHTTP");
+				}
+			} 
+			else 
+			{
+				xhr = new XMLHttpRequest(); 
+			}
+		} 
+		else 
+		{
+			alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+			return null;
+		}
+	
+		return xhr;
+	}
+	
 	//A terminer par rapport au formulaire
 	$('#valider').mousedown(function()
 	{
+		var xhr = getXMLHttpRequest();
 		if($('#other-date').is(':checked'))
 		{
-			var datedebut = $('#date_debut').val();
-			var datefin = $('#date_fin').val();
+			var datedebut = encodeURIComponent($('#date_debut').val());
+			var datefin = encodeURIComponent(($('#date_fin').val()));
 			console.log(dateTotimeStamp(datedebut));
 			console.log(dateTotimeStamp(datefin));
 		}
@@ -35,7 +78,10 @@ $(function () {
 			var dateannee = "01-" + "01-" + date;
 			console.log(dateTotimeStamp(dateannee));
 		}
-		if($(''))
+	
+		xhr.open("GET", "handlingData.php?date_debut" + datedebut + "&date_fin=" + datefin, true);
+		xhr.send(null);
+		
 	});
 	
 	
